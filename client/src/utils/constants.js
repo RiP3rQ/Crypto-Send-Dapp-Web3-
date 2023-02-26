e4747a9f7a150886c0949435ef01e1c45a9a0675
RiP3rQ <rafalpompa2000@gmail.com>
2023-02-27 00:40:09 +0100

    deployed smart contract using hardhat and alchemy

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractABI = abi.abi;
+export const contractAddress = "0xf80D498cc3C6c3a677d637FEB475f148d48c333f";
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -2,5 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.0",
+  networks: {
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/EgbLHbEPKXwngaugAacNsXzHMk1pSUzW",
+      accounts: [
+        "a1ddde1e321877facbc1179997a7f734ec1770b19537a22c5c3b1851c2bb958a",
+      ],
+    },
+  },
 };
