9108896a1e5169d9f2cab197da83a5bd2dd3e696
RiP3rQ <rafalpompa2000@gmail.com>
2023-02-28 22:10:07 +0100

    fixes + working eth sending

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState, createContext } from "react";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 
 import { contractABI, contractAddress } from "../utils/constants";
 
-export const TransactionContext = createContext();
+export const TransactionContext = React.createContext();
 
 const { ethereum } = window;
 
